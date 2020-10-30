34fc6f7e3f41f93faeed86369b1194422764c812
kristian240 <kristian.djakovic@infinum.hr>
2020-10-30 20:53:07 +0100

    Add chakra-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { ChakraProvider, extendTheme, Box, Text } from '@chakra-ui/core';
+
 import logo from './logo.svg';
-import './App.css';
+import customTheme from './styles/theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={extendTheme(customTheme)}>
+      <div className="App">
+        <Box bgColor="blue.100" py={3}>
+          <Text fontSize="lg">Parkiraj me</Text>
+        </Box>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
