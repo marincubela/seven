a9650cb96af4df5d0eb7fc71b19eceed3b03ee19
kristian240 <kristian.djakovic@infinum.hr>
2020-11-01 17:41:03 +0100

    Update theme js

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -1,3 +1,27 @@
-const theme = {};
+const theme = {
+  color: {
+    primary: {
+      50: 'blue.50',
+      100: 'blue.100',
+      200: 'blue.200',
+      300: 'blue.300',
+      400: 'blue.400',
+      500: 'blue.500',
+      600: 'blue.600',
+      700: 'blue.700',
+      800: 'blue.800',
+      900: 'blue.900',
+    },
+  },
+  components: {
+    Link: {
+      baseStyle: {
+        '&[aria-current]': {
+          fontWeight: 'bold',
+        },
+      },
+    },
+  },
+};
 
 export default theme;
