058d2551cfdce8c6b79ee37c4e1e9779bf3f74a2
Marin Ćubela <mc51493@fer.hr>
2020-11-06 01:41:03 +0100

    Add not working user router

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -3,9 +3,64 @@ const session = require('express-session');
 const { body, validationResult } = require('express-validator');
 
 const Racun = require('../models/Racun');
+const Klijent = require('../models/Klijent');
 const { arePasswordEqual } = require('../utils/password');
 
 const router = express.Router();
+
+/*
+  This endpoint will return a user with a given id
+
+  If user is logged, this will retun with status 200 and:
+  {
+    data: {
+      user: {
+        
+      }
+    }
+  }
+
+  If user is not logged, this will return with status 401 and:
+  {
+    error: {
+      message: string,
+    }
+  }
+*/
+router.get('/:id', async (req, res) => {
+  // Kako se ovo radi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  if (!(req.session.user.admin || req.session.user.id === req.query.id)) {
+    return res.status(401).json({
+      error: {
+        message: 'Neovlaštena radnja',
+      },
+    });
+  }
+
+  // find user by id and return it
+  const id = req.query.id;
+  const user = await Racun.findOne({
+    where: {
+      id,
+    },
+  });
+
+  console.log(user.toJSON());
+
+  if (!user) {
+    return res.json({
+      error: {
+        message: `Korisnik s id=${id} ne postoji`,
+      },
+    });
+  }
+
+  return res.json({
+    data: {
+      user: user.toJSON(), //return it here!,
+    },
+  });
+});
 
 /*
   This endpoint will return all users in app
@@ -39,58 +94,7 @@ router.get('/', async (req, res) => {
   // find user by id and retunt it
   return res.json({
     data: {
-      users: 2, //return it here!,
-    },
-  });
-});
-
-/*
-  This endpoint will return a user with a given id
-
-  If user is logged, this will retun with status 200 and:
-  {
-    data: {
-      user: {
-        
-      }
-    }
-  }
-
-  If user is not logged, this will return with status 401 and:
-  {
-    error: {
-      message: string,
-    }
-  }
-*/
-router.get('/:id', async (req, res) => {
-  if (!(req.session.user.admin || req.session.user.id === req.query.id)) {
-    return res.status(401).json({
-      error: {
-        message: 'Neovlaštena radnja',
-      },
-    });
-  }
-
-  // find user by id and return it
-  const id = req.query.id;
-  const user = await Racun.findOne({
-    where: {
-      id,
-    },
-  });
-
-  if (!user) {
-    return res.json({
-      error: {
-        message: `Korisnik s id=${id} ne postoji`,
-      },
-    });
-  }
-
-  return res.json({
-    data: {
-      user: user.toJSON(), //return it here!,
+      users: null, //return it here!,
     },
   });
 });
