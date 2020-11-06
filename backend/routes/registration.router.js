a66d27af2c99a9929e0206610c0ec2fc4865b85f
Marin Ćubela <mc51493@fer.hr>
2020-11-06 18:09:05 +0100

    Fixed company registration

--- backend/routes/registration.router.js
+++ backend/routes/registration.router.js
@@ -19,7 +19,13 @@ const createAccount = async ({ email, oib, password }) => {
 };
 
 //Will create a Tvrtka model
-const createCompany = (accountData) => {};
+const createCompany = async ({ companyName, adress, racunId }) => {
+  const company = await Tvrtka.create({
+    naziv: companyName,
+    adresa: adress,
+    racunId,
+  });
+};
 
 //Will create a Klijent model
 const createClient = async ({
@@ -73,6 +79,7 @@ router.post(
     // email field needs to be an email
     body('data.email')
       .isEmail()
+      .withMessage('Email je neispravan')
       .custom((email) =>
         emailCheck(email).then((emailExist) => {
           if (emailExist) {
@@ -82,12 +89,14 @@ router.post(
       ),
 
     // password field is required
-    body('data.password').not().isEmpty(),
+    body('data.password').not().isEmpty().withMessage('Lozinka je prazna'),
 
     // OIB field length must be 11
     body('data.oib')
-      .isLength({ min: 11, max: 11 })
       .isNumeric()
+      .withMessage('OIB mora imati samo brojeve')
+      .isLength({ min: 11, max: 11 })
+      .withMessage('OIB mora imati 11 brojeva')
       .custom((oib) =>
         oibCheck(oib).then((oibExist) => {
           if (oibExist) {
@@ -167,26 +176,70 @@ router.post(
   '/company',
   [
     // email field needs to be an email
-    body('data.email').isEmail(),
+    body('data.email')
+      .isEmail()
+      .withMessage('Email je neispravan')
+      .custom((email) =>
+        emailCheck(email).then((emailExist) => {
+          if (emailExist) {
+            return Promise.reject('Račun u uporabi');
+          }
+        })
+      ),
 
     // password field is required
-    body('data.password').not().isEmpty(),
+    body('data.password').not().isEmpty().withMessage('Lozinka je prazna'),
 
     // OIB field length must be 11
-    body('data.OIB').isLength({ min: 11, max: 11 }).isNumeric(),
+    body('data.oib')
+      .isNumeric()
+      .withMessage('OIB mora imati samo brojeve')
+      .isLength({ min: 11, max: 11 })
+      .withMessage('OIB mora imati 11 brojeva')
+      .custom((oib) =>
+        oibCheck(oib).then((oibExist) => {
+          if (oibExist) {
+            return Promise.reject('Račun u uporabi');
+          }
+        })
+      ),
 
-    // Name field must consists of letters
-    body('data.name').isAlpha(),
+    // Company name field is required
+    body('data.companyName')
+      .not()
+      .isEmpty()
+      .withMessage('Ime tvrtke je prazno'),
 
-    // Surname field must consists of letters
-    body('data.surname').isAlpha(),
-
-    // Credit card must consists of 16 numbers
-    body('data.creditCard').isLength({ min: 16, max: 16 }).isNumeric(),
+    // Adress field is required
+    body('data.adress').not().isEmpty().withMessage('Adresa je prazna'),
   ],
   async (req, res, next) => {
-    const emailCheck = await Racun.findOne({
-      where: emailCheck,
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, oib, companyName, password, adress } = req.body.data;
+
+    var user = await createAccount({ email, oib, password });
+
+    await createCompany({
+      adress,
+      companyName,
+      racunId: user.id,
+    });
+
+    req.session.user = {
+      id: user.id,
+      email: user.email,
+      admin: user.admin,
+    };
+
+    return res.status(200).json({
+      data: {
+        user: req.session.user,
+      },
     });
   }
 );
