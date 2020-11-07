ea69013c693875709248642238f67e7468881a63
Marin Ćubela <mc51493@fer.hr>
2020-11-07 21:07:43 +0100

    Fixed user route

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,102 +1,236 @@
 const express = require('express');
-const session = require('express-session');
-const { body, validationResult } = require('express-validator');
 
 const Racun = require('../models/Racun');
 const Klijent = require('../models/Klijent');
-const { arePasswordEqual } = require('../utils/password');
+const Tvrtka = require('../models/Tvrtka');
 
 const router = express.Router();
 
 /*
+  Returns a promise, resolves if user with given id exists, rejects if not
+  If resolved returns
+  {
+    user,
+    isClient
+  }
+*/
+const getUserById = async (id) => {
+  return new Promise(async (resolve, reject) => {
+    // find account by id
+    const racun = await Racun.findOne({
+      where: {
+        id,
+      },
+    });
+
+    // reject with status 400 if account doesn't exist
+    if (!racun) {
+      reject({
+        errors: [
+          {
+            message: `Korisnik s id=${id} ne postoji`,
+          },
+        ],
+        status: 404,
+      });
+    }
+
+    // filter data for returning user
+    const user = {
+      id: racun.id,
+      email: racun.email,
+      OIB: racun.OIB,
+      admin: racun.admin,
+    };
+
+    // fetch client by account id
+    const klijent = await Klijent.findOne({
+      where: {
+        racunId: racun.id,
+      },
+    });
+
+    // if client exists, resolve with clients data
+    if (klijent) {
+      user.ime = klijent.ime;
+      user.prezime = klijent.prezime;
+      user.brojKartice = klijent.brojKartice;
+      resolve({ user, isClient: true });
+    }
+
+    // fetch company by account id
+    const tvrtka = await Tvrtka.findOne({
+      where: {
+        racunId: racun.id,
+      },
+    });
+
+    // if company exists, resolve with companies data
+    if (tvrtka) {
+      user.naziv = tvrtka.naziv;
+      user.adresa = tvrtka.adresa;
+      resolve({ user, isClient: false });
+    }
+
+    // if client nor company exists and account does, there is error in database
+    reject({
+      errors: [
+        {
+          message: 'Internal server error',
+        },
+      ],
+      status: 500,
+    });
+  });
+};
+
+/*
+  Returns all users 
+  {
+    clients: [],
+    companies: [],
+  }
+*/
+const getAllUsers = async () => {
+  return new Promise(async (resolve, reject) => {
+    const numberOfAccounts = await Racun.count();
+    let i = 0,
+      id = 1;
+    var users = { clients: [], companies: [] };
+
+    while (i < numberOfAccounts) {
+      await getUserById(id)
+        .then(({ user, isClient }) => {
+          console.log('User fetched:', user);
+          if (isClient) {
+            users.clients.push(user);
+          } else {
+            users.companies.push(user);
+          }
+          i++;
+        })
+        .catch(({ errors, status }) => {
+          if (status === 404) {
+            console.log(`Account with id = ${id} doesn't exist!`);
+          } else if (status === 500) {
+            console.log(errors[0].message);
+          }
+        });
+      id++;
+    }
+
+    resolve(users);
+  });
+};
+
+/**
   This endpoint will return a user with a given id
 
   If user is logged, this will retun with status 200 and:
+  for clients:
   {
     data: {
       user: {
-        
+        id: integer,
+        email: string,
+        OIB: string,
+        admin: boolean
+        ime: string,
+        prezime: string,
+        brojKartice: string
+      }
+    }
+  }
+
+  for companies:
+  {
+    data: {
+      user: {
+        id: integer,
+        email: string,
+        OIB: string,
+        admin: boolean,
+        naziv: string,
+        adresa: string
       }
     }
   }
 
   If user is not logged, this will return with status 401 and:
   {
-    error: {
+    errors: [
+      {
       message: string,
     }
+  ]
   }
 */
 router.get('/:id', async (req, res) => {
-  // Kako se ovo radi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  if (!(req.session.user.admin || req.session.user.id === req.query.id)) {
+  if (!(req.session.user.admin || req.session.user.id == req.params.id)) {
     return res.status(401).json({
-      error: {
-        message: 'Neovlaštena radnja',
-      },
+      errors: [
+        {
+          message: 'Neovlaštena radnja',
+        },
+      ],
     });
   }
 
-  // find user by id and return it
-  const id = req.query.id;
-  const user = await Racun.findOne({
-    where: {
-      id,
-    },
-  });
-
-  console.log(user.toJSON());
-
-  if (!user) {
-    return res.json({
-      error: {
-        message: `Korisnik s id=${id} ne postoji`,
-      },
+  await getUserById(req.params.id)
+    .then(({ user }) => {
+      return res.json({
+        data: {
+          user,
+        },
+      });
+    })
+    .catch(({ errors, status }) => {
+      return res.status(status).json({
+        errors,
+      });
     });
-  }
-
-  return res.json({
-    data: {
-      user: user.toJSON(), //return it here!,
-    },
-  });
 });
 
 /*
-  This endpoint will return all users in app
+  This endpoint will return all users in app.
 
-  If user is logged, this will retun with status 200 and:
+  If user is admin, this will return with status 200 and:
   {
     data: {
-      user: {
-        email: string,
-        password: string,
-      }
+      clients[],
+      companies[]
     }
   }
 
-  If user is not logged, this will return with status 401 and:
+  If user is not admin 
   {
-    error: {
+    errors: [{
       message: string,
-    }
+    }]
   }
 */
 router.get('/', async (req, res) => {
   if (!req.session.user.admin) {
     return res.status(401).json({
-      error: {
-        message: 'Neovlaštena radnja',
-      },
+      errors: [
+        {
+          message: 'Neovlaštena radnja',
+        },
+      ],
     });
   }
 
-  // find user by id and retunt it
-  return res.json({
-    data: {
-      users: null, //return it here!,
-    },
-  });
+  await getAllUsers()
+    .then((users) => {
+      return res.json({
+        data: {
+          users,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
