f79dad04c335455084b269582140e7c7d27be9c4
kpetkovic <kristina.petkovic@fer.hr>
2020-12-15 13:39:29 +0100

    add VoziloMapper

---
+++ backend/mappers/VoziloMapper.ts
@@ -0,0 +1,30 @@
+import { Vozilo, IVoziloAttributes } from '../models/Vozilo';
+import { VoziloDTO } from '../dtos/VoziloDTO';
+import { Mapper } from './Mapper';
+
+export class VoziloMapper extends Mapper {
+  public static toDomain(voziloDTO: VoziloDTO): IVoziloAttributes {
+    return {
+        idVozilo: voziloDTO.idVozilo,
+        registracija: voziloDTO.registration,
+        nazivVozila: voziloDTO.carName,
+        boja: voziloDTO.color
+    };
+  }
+
+  public static async toDTO(vozilo: Vozilo): Promise<VoziloDTO> {
+    const klijent = await vozilo.getKlijent();
+    const racun = await klijent.getRacun();
+
+    return {
+        idRacun: racun.idRacun,
+        registration: vozilo.registracija,
+        carName: vozilo.nazivVozila,
+        color: vozilo.boja,
+        firstName: klijent.ime,
+        lastName:klijent.prezime,
+        cardNumber: klijent.brojKartice,
+        email:racun.email
+    };
+  }
+}
