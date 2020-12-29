b5ba92278ed5ebf877708d79715f351f1773f492
kpetkovic <kristina.petkovic@fer.hr>
2020-12-29 21:20:50 +0100

    Finish VoziloController

--- backend/controllers/vozilo/VoziloController.ts
+++ backend/controllers/vozilo/VoziloController.ts
@@ -1,9 +1,15 @@
 import { CreateVoziloController } from './CreateVoziloController';
 import { DeleteVoziloController } from './DeleteVoziloController';
 import { GetVoziloController } from './GetVoziloController';
+import { GetAllVoziloController } from './GetAllVoziloController';
+import { GetFromClientVoziloController } from './GetFromClientVoziloController';
 
 export class VoziloController {
   public static create = new CreateVoziloController().execute;
   public static delete = new DeleteVoziloController().execute;
   public static get = new GetVoziloController().execute;
+  public static getAll = new GetAllVoziloController().execute;
+
+  public static getFromCompany = new GetFromClientVoziloController()
+    .execute;
 }
