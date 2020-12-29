0d1bc2eb673b57f5dd3f8283e929bf6fbce6e704
Marin Ćubela <mc51493@fer.hr>
2020-12-29 10:57:47 +0100

    Add imports

--- backend/controllers/parkiraliste/index.ts
+++ backend/controllers/parkiraliste/index.ts
@@ -1,1 +1,6 @@
 export * from './CreateParkiralisteController';
+export * from './DeleteParkiralisteController';
+export * from './GetAllParkiralisteController';
+export * from './GetFromCompanyParkiralisteController';
+export * from './GetParkiralisteController';
+export * from './ParkiralisteController';
