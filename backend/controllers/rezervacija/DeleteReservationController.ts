f93e23653ccddab5e2654dfb5584ff7a63551a79
Andrea <andrea.kristo@fer.hr>
2021-01-02 14:16:32 +0100

    Add DeleteReservationController

--- backend/controllers/rezervacija/DeleteReservationController.ts
+++ backend/controllers/rezervacija/DeleteReservationController.ts
@@ -1,25 +1,28 @@
-import { BaseController } from "../BaseController";
-import { IResponse } from "../../interfaces/network";
-import { IRequest } from "../../interfaces/network";
+import { BaseController } from '../BaseController';
+import { IResponse } from '../../interfaces/network';
+import { IRequest } from '../../interfaces/network';
+import { RezervacijaRepo } from '../../repos/RezervacijaRepo';
 
 export class DeleteReservationController extends BaseController {
-    executeImpl = async (
-        req: IRequest,
-        res: IResponse
-    ): Promise<void | IResponse> => {
-        const idReservartion = Number(req.params.idReservation);
+  executeImpl = async (
+    req: IRequest,
+    res: IResponse
+  ): Promise<void | IResponse> => {
+    const idRezervacija = Number(req.params.idReservation);
 
-        if(isNaN(idReservartion)){
-            return this.clientError(res, ['Id rezervacije nije broj!']);
-        }
-
-        if(idReservartion < 1){
-            return  this.clientError(res, ['Id rezervacije ne smije biti negativan broj!']);
-        }
-
-      
-
-        return this.ok(res);
+    if (isNaN(idRezervacija)) {
+      return this.clientError(res, ['Id rezervacije nije broj!']);
     }
-    
+
+    if (idRezervacija < 1) {
+      return this.clientError(res, [
+        'Id rezervacije ne smije biti negativan broj!',
+      ]);
+    }
+
+    //obrisi
+    await RezervacijaRepo.deleteByIdRezervacija(idRezervacija);
+
+    return this.ok(res);
+  };
 }
