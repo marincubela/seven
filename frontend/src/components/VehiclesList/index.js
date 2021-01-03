32fecaa2cb2ef6ae275326de376c44525c7c5ed7
Marko Bakic <marko.bakic@fer.hr>
2021-01-03 12:29:03 +0100

    Get request for client vehicles

--- frontend/src/components/VehiclesList/index.js
+++ frontend/src/components/VehiclesList/index.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import { Box, Button, Heading, Link } from '@chakra-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Box, Text, Button, Heading, Link } from '@chakra-ui/core';
 import { Link as ReactLink } from 'react-router-dom';
 import { get } from '../../utils/network';
 import { useStore } from '../../store/StoreProvider';
 
 export function VehiclesList() {
-  var vehicles;
+  const [vehicles, setVehicles] = useState([]);
 
   const store = useStore();
 
-  get(`vehicle/client/${store.currentUser.idRacun}`)
-    .then((res) => {
-      if (res.data && res.data.vehicles) {
-        vehicles = res.data.vehicles;
-        console.log(vehicles);
-      }
-    })
-    .catch((res) => {
-      console.log('erorrrrrrr');
-      console.log(res);
-      // if (true) {
-      //   // setErrormessage
-      // }
-    });
-
-  // const items = [];
-
-  // for (const voz in vehicles) {
-  //   var el = <li>voz</li>;
-
-  //   items.push(el);
-  // }
+  // console.log(store.currentUser.idRacun);
+  useEffect(() => {
+    get(`vehicle/client/${store.currentUser.idRacun}`)
+      .then((res) => {
+        if (res.data && res.data.vehicles) {
+          setVehicles(res.data.vehicles);
+          console.log(res.data.vehicles);
+        }
+      })
+      .catch((res) => {
+        console.log('erorrrrrrr');
+        console.log(res);
+      });
+  }, []);
 
   return (
     <Box>
       <Heading>vozila</Heading>
-      {/* <Box>{vehicles}</Box> */}
+
+      <Box>
+        {vehicles.map((veh) => (
+          <Box key={veh.idVozilo}>
+            <Text>{veh.color}</Text>
+          </Box>
+        ))}
+      </Box>
       <Button>
         <Link as={ReactLink} to="/vehicles/add" flex="1">
           dodaj
