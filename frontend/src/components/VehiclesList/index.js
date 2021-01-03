124cc71ebdd9d18fec5ede96b8585b56bf4d41ec
Marko Bakic <marko.bakic@fer.hr>
2021-01-03 15:51:18 +0100

    Update get request

--- frontend/src/components/VehiclesList/index.js
+++ frontend/src/components/VehiclesList/index.js
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Text, Button, Heading, Link } from '@chakra-ui/core';
-import { Link as ReactLink } from 'react-router-dom';
+import { Link as ReactLink, useHistory } from 'react-router-dom';
 import { get } from '../../utils/network';
 import { useStore } from '../../store/StoreProvider';
 
 export function VehiclesList() {
+  const history = useHistory();
   const [vehicles, setVehicles] = useState([]);
 
   const store = useStore();
 
-  // console.log(store.currentUser.idRacun);
   useEffect(() => {
-    get(`vehicle/client/${store.currentUser.idRacun}`)
-      .then((res) => {
-        if (res.data && res.data.vehicles) {
-          setVehicles(res.data.vehicles);
-          console.log(res.data.vehicles);
-        }
-      })
-      .catch((res) => {
-        console.log('erorrrrrrr');
-        console.log(res);
-      });
+    if (store.currentUser) {
+      get(`vehicle/client/${store.currentUser.idRacun}`)
+        .then((res) => {
+          if (res.data && res.data.vehicles) {
+            setVehicles(res.data.vehicles);
+            console.log(res.data.vehicles);
+          }
+        })
+        .catch((res) => {
+          console.log('erorrrrrrr');
+          console.log(res);
+        });
+    } else {
+      history.replace('/');
+    }
   }, []);
 
   return (
@@ -31,13 +35,13 @@ export function VehiclesList() {
       <Box>
         {vehicles.map((veh) => (
           <Box key={veh.idVozilo}>
-            <Text>{veh.color}</Text>
+            <Text>{veh.carName}</Text>
           </Box>
         ))}
       </Box>
       <Button>
         <Link as={ReactLink} to="/vehicles/add" flex="1">
-          dodaj
+          Dodaj
         </Link>
       </Button>
     </Box>
