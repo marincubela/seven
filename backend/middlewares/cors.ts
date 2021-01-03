c58ce0ef638adfb56080cc5c86e1d6bc61caeaa7
kristian240 <kristian.djakovic@infinum.hr>
2021-01-03 16:54:24 +0100

    Update allowedOrigins

--- backend/middlewares/cors.ts
+++ backend/middlewares/cors.ts
@@ -4,17 +4,14 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3000/',
 
-  'http://frontend-local.herokuapp.com:3000',
-  'http://frontend-local.herokuapp.com:3000/',
-
   'https://pi-parkiraj-me-frontend.herokuapp.com',
   'https://pi-parkiraj-me-frontend.herokuapp.com/',
 
   'http://app.parkirajme.xyz',
   'http://app.parkirajme.xyz/',
 
-  'http://frontend-local.parkirajme.xyz',
-  'http://frontend-local.parkirajme.xyz/',
+  'http://frontend-local.parkirajme.xyz:3000',
+  'http://frontend-local.parkirajme.xyz:3000/',
 ];
 
 export const corsMiddleware = cors({
