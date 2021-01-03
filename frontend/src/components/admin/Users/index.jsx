20d499022e4adfa65f029518d7a2be90593aebe0
kristian240 <kristian.djakovic@infinum.hr>
2021-01-03 18:47:50 +0100

    Add user table

--- frontend/src/components/admin/Users/index.jsx
+++ frontend/src/components/admin/Users/index.jsx
@@ -1,5 +1,75 @@
-import React from 'react';
+import { Button, Center, Spinner, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/core';
+import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 
-export const Users = ({ ...rest }) => {
-  return <div {...rest}>Users</div>;
+import { get } from '../../../utils/network';
+
+export const Users = (props) => {
+  const [users, setUsers] = useState();
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    get('user/all')
+      .then((res) => setUsers(res.data.users))
+      .catch(() => {
+        setError(true);
+      });
+  }, []);
+
+  console.log(users);
+
+  if (error) {
+    return <Center {...props}>Dogodila se pogreška</Center>;
+  }
+
+  if (!users) {
+    return (
+      <Center {...props}>
+        <Spinner />
+      </Center>
+    );
+  }
+
+  return (
+    <Tabs {...props}>
+      <TabList>
+        <Tab>Klijenti</Tab>
+        <Tab>Tvrtke</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel width="full">
+          <Table variant="striped">
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Ime</Th>
+                <Th>Prezime</Th>
+                <Th>E-mail</Th>
+                <Th />
+              </Tr>
+            </Thead>
+            <Tbody>
+              {users.clients.map(({ idRacun, firstName, lastName, email }) => (
+                <Tr key={idRacun}>
+                  <Td>{idRacun}</Td>
+                  <Td>{firstName}</Td>
+                  <Td>{lastName}</Td>
+                  <Td>{email}</Td>
+                  <Td>
+                    <Button>Delete</Button>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TabPanel>
+        <TabPanel>
+          {users.companies.map(({ idRacun, name }) => (
+            <Text key={idRacun}>{name}</Text>
+          ))}
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
 };
