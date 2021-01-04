032f667a6015250d4b35429d5b0d4b5dfc817937
Marko Bakic <marko.bakic@fer.hr>
2021-01-04 15:21:16 +0100

    Vehicles view and delete function

--- frontend/src/components/VehiclesList/index.js
+++ frontend/src/components/VehiclesList/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Text, Button, Heading, Link } from '@chakra-ui/core';
+import { Box, Text, Button, Heading, Link, HStack, Divider, IconButton } from '@chakra-ui/core';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
 import { Link as ReactLink, useHistory } from 'react-router-dom';
-import { get } from '../../utils/network';
+import { get, destroy } from '../../utils/network';
 import { useStore } from '../../store/StoreProvider';
 
 export function VehiclesList() {
@@ -29,28 +30,67 @@ export function VehiclesList() {
     }
   }, []);
 
-  //registration: string;
+  // registration: string;
   // carName: string;
   // color: boolean;
+
+  function handleDelete(idVehicle) {
+    destroy(`vehicle/${idVehicle}`)
+      .then(() => {
+        history.replace('/');
+        // history.go(0);
+      })
+      .catch((err) => {
+        console.log('error');
+        console.log(err);
+      });
+  }
+
   return (
     <Box bgColor="primary.200" marginY="8" padding="6" borderRadius="lg">
-      <Heading as="h2" size="xl" marginY="4">
+      <Heading as="h2" size="xl" marginY="4" textAlign="center">
         vozila
       </Heading>
 
-      {vehicles
-        ? vehicles.map((veh) => (
-            <Box key={veh.idVozilo}>
-              <Text>{veh.color}</Text>
+      {vehicles.map((veh) => (
+        <Box key={veh.idVozilo} h={10} marginY="4" borderRadius="lg" background="white">
+          <HStack h="inherit">
+            <Box
+              bgColor={`${veh.color}`}
+              w="10%"
+              h={10}
+              borderBottomLeftRadius="base"
+              borderTopLeftRadius="base"
+              marginRight={4}
+            />
+            <Box w="30%">
+              <Text>{veh.carName}</Text>
             </Box>
-          ))
-        : null}
-
-      <Button>
-        <Link as={ReactLink} to="/vehicles/add" flex="1">
-          Dodaj
-        </Link>
-      </Button>
+            {/* <Divider orientation="vertical" bgColor="primary.200" w={1} /> */}
+            <Box w="30%">
+              <Text>{veh.registration}</Text>
+            </Box>
+            {/* <Divider orientation="vertical" bgColor="primary.200" w={1} /> */}
+            <Box w="10%" />
+            <IconButton aria-label="Edit vehicle" icon={<EditIcon />} />
+            <IconButton
+              aria-label="Delete vehicle"
+              icon={<DeleteIcon />}
+              onClick={() => {
+                handleDelete(veh.idVozilo);
+              }}
+            />
+          </HStack>
+          {/* <Divider orientation="horizontal" height={2} bgColor="primary.200" /> */}
+        </Box>
+      ))}
+      <Box flex="1" alignItems="center" display="flex">
+        <Button margin="auto">
+          <Link as={ReactLink} to="/vehicles/add">
+            Dodaj
+          </Link>
+        </Button>
+      </Box>
     </Box>
   );
 }
