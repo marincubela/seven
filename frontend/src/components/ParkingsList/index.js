610d95bd7989dd0e15e38a742524b96c26b5b80e
Lucija <lucija.strejcek@fer.hr>
2021-01-04 16:11:09 +0100

    Parkings view and delete function

--- frontend/src/components/ParkingsList/index.js
+++ frontend/src/components/ParkingsList/index.js
@@ -1,7 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Heading, Link, Text } from '@chakra-ui/core';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Button,
+  Link,
+  Spacer,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import { DeleteIcon, EditIcon, AddIcon, CloseIcon } from '@chakra-ui/icons';
 import { Link as ReactLink, useHistory } from 'react-router-dom';
-import { get } from '../../utils/network';
+import { destroy, get } from '../../utils/network';
 import { useStore } from '../../store/StoreProvider';
 
 export function ParkingsList() {
@@ -11,13 +32,18 @@ export function ParkingsList() {
 
   const history = useHistory();
 
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef();
+  const [deleteParking, setdeleteParkingking] = useState([]);
+
   useEffect(() => {
     if (store.currentUser) {
       get(`parking?company=${store.currentUser.idRacun}`)
         .then((res) => {
           if (res.data && res.data.parkings) {
             setParkings(res.data.parkings);
-            console.log(res.data.parkings);
+            //console.log(res.data.parkings);
           }
         })
         .catch((res) => {
@@ -29,21 +55,100 @@ export function ParkingsList() {
     }
   }, []);
 
+  function handleDelete(id) {
+    destroy(`parking/${id}`)
+      .then(() => {
+        setIsOpen(false);
+        history.replace('/');
+      })
+      .catch((err) => {
+        console.log('eror');
+        console.log(err);
+      });
+  }
+
   return (
     <Box>
-      <Heading>Parkirališta</Heading>
-      <Box>
-        {parkings.map((p) => (
-          <Box key={p.idParkiraliste}>
-            <Text>{p.parkingName}</Text>
-          </Box>
-        ))}
-      </Box>
-      <Button>
-        <Link as={ReactLink} to="/parkings/add" flex="1">
-          Dodaj
-        </Link>
-      </Button>
+      <Flex>
+        <Heading>Moja parkirališta</Heading>
+        <Spacer />
+        <Button aria-label="Add parking" leftIcon={<AddIcon />} size="lg">
+          <Link as={ReactLink} to="/parkings/add" flex="1">
+            Dodaj
+          </Link>
+        </Button>
+      </Flex>
+
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Naziv</Th>
+            <Th isNumeric>Broj mjesta</Th>
+            <Th isNumeric>Broj invalidskih mjesta</Th>
+            <Th>Tip parkirališta</Th>
+            <Th isNumeric>Jednokratna rezervacija</Th>
+            <Th isNumeric>Ponavljajuća rezervacija</Th>
+            <Th isNumeric>Trajna rezervacija</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {parkings.map((p) => (
+            <Tr key={p.idParkiraliste}>
+              <Td>{p.parkingName}</Td>
+              <Td isNumeric>{p.capacity}</Td>
+              <Td isNumeric>{p.disabledCapacity}</Td>
+              <Td>{p.parkingType}</Td>
+              <Td isNumeric>{p.oneTimePrice} kn</Td>
+              <Td isNumeric>{p.repetitivePrice} kn</Td>
+              <Td isNumeric>{p.permanentPrice} kn</Td>
+              <Td>
+                <HStack align="center">
+                  <Button aria-label="Edit parking" leftIcon={<EditIcon />}>
+                    Uredi
+                  </Button>
+                  <Button
+                    aria-label="Delete parking"
+                    leftIcon={<DeleteIcon />}
+                    onClick={() => {
+                      setIsOpen(true);
+                      setdeleteParkingking(p);
+                    }}
+                  >
+                    Izbriši
+                  </Button>
+                </HStack>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+        <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Izbriši parkiralište
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                Jeste li sigurni da želite izbrisati parkiralište {deleteParking.parkingName}?
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button leftIcon={<CloseIcon />} ref={cancelRef} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button
+                  leftIcon={<DeleteIcon />}
+                  colorScheme="red"
+                  onClick={() => handleDelete(deleteParking.idParkiraliste)}
+                  ml={3}
+                >
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </Table>
     </Box>
   );
 }
