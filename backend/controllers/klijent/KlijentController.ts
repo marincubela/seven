e3ddde710446a95e90cde6ae08bf7e252ada414d
Kristina Petkovic <kristina.petkovic@fer.hr>
2021-01-04 11:45:39 +0100

    Add patch route

--- backend/controllers/klijent/KlijentController.ts
+++ backend/controllers/klijent/KlijentController.ts
@@ -1,5 +1,7 @@
 import { CreateKlijentController } from './CreateKlijentController';
+import { UpdateKlijentController } from './UpdateKlijentController';
 
 export class KlijentController {
   public static create = new CreateKlijentController().execute;
+  public static update=new UpdateKlijentController().execute;
 }
--- backend/routes/user.router.ts
+++ backend/routes/user.router.ts
@@ -1,5 +1,6 @@
-import { KlijentController } from '../controllers/klijent/KlijentController';
+
 import { UserController } from '../controllers/user/UserController';
+import { KlijentController } from '../controllers/klijent/KlijentController';
 import express from 'express';
 
 export const userRouter = express.Router();
@@ -8,3 +9,7 @@ userRouter.delete('/:idRacun', UserController.delete);
 userRouter.get('/all', UserController.getAll);
 
 userRouter.get('/:idRacun', UserController.get);
+
+userRouter.patch('/client/update', KlijentController.update);
+
+
