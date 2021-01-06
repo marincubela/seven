13f7c12a7939aafb5fc77837c5062ac5755ca476
Kristina Petkovic <kristina.petkovic@fer.hr>
2021-01-06 13:01:31 +0100

    Fix PonavljajucaValidator

--- backend/utils/validators/PonavljajucaValidator.ts
+++ backend/utils/validators/PonavljajucaValidator.ts
@@ -3,11 +3,11 @@ import * as yup from 'yup';
 import { BaseValidator } from './BaseValidator';
 
 export class PonavljajucaValidator extends BaseValidator {
-  static shema = yup.object().shape({
-    datumRezervacije: yup.date().required('Datum rezervacije je obavezno'),
-    datumKrajaRez: yup.date().required('Datum kraja rezervacije je obavezno'),
-    daniPonavljanja: yup.date().required('Dani ponavljanja su obavezni'),
-    vrijemePocetka: yup.date().required('Vrijeme pocetka je obavezno!'),
-    vrijemeKraja: yup.date().required('Vrijeme kraja je obavezno!'),
+  static schema = yup.object().shape({
+    startTime: yup.string().required('Vrijeme pocetka je obavezno!'),
+    endTime: yup.string().required('Vrijeme kraja je obavezno!'),
+    repeatDays: yup.number().required('Dani ponavljanja su obavezni'),
+    reservationDate: yup.date().required('Datum rezervacije je obavezno'),
+    reservationEndDate: yup.date().required('Datum kraja rezervacije je obavezno'),
   });
 }
