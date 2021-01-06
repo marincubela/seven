0ba955924dfdc3c7ab0b72c4b35c2d7f87838ee2
kristian240 <kristian.djakovic@infinum.hr>
2021-01-06 16:45:43 +0100

    Remove pocetna from admin panel

--- frontend/src/Admin.jsx
+++ frontend/src/Admin.jsx
@@ -1,7 +1,7 @@
 import { Divider } from '@chakra-ui/react';
 import { observer } from 'mobx-react';
 import React, { Fragment } from 'react';
-import { Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
 
 import { AdminContainer } from './components/admin/Container';
 import { AdminNavigation } from './components/admin/Navigation';
@@ -28,14 +28,12 @@ export const AdminRouter = observer(() => {
       <AdminContainer>
         <Switch>
           <Route path={`${path}/`} exact>
-            Admin panel
+            <Redirect to={`${path}/users`} />
           </Route>
 
           <Route path={`${path}/users`}>
             <Users />
           </Route>
-
-          <Route path={`${path}/parkings`}>Parkings | Admin panel</Route>
         </Switch>
       </AdminContainer>
     </Fragment>
