6c6f260c42da7ec3bda35d27fcab8216e23f582a
Marin Ćubela <mc51493@fer.hr>
2021-01-07 22:49:31 +0100

    Fix get from clientReservation

--- backend/controllers/rezervacija/GetFromClientReservation.ts
+++ backend/controllers/rezervacija/GetFromClientReservation.ts
@@ -2,6 +2,7 @@ import { BaseController } from '../BaseController';
 import { IResponse } from '../../interfaces/network';
 import { IRequest } from '../../interfaces/network';
 import { RezervacijaRepo } from '../../repos/RezervacijaRepo';
+import { KlijentRepo } from '../../repos/KlijentRepo';
 
 export class GetFromClientReservation extends BaseController {
   executeImpl = async (
@@ -18,7 +19,10 @@ export class GetFromClientReservation extends BaseController {
       return this.clientError(res, ['Id mora biti pozitivan broj']);
     }
 
-    if (idKlijent != req.session.user.idRacun) {
+    if (
+      (await KlijentRepo.getIdRacunByIdKlijent(idKlijent)) !=
+      req.session.user.idRacun
+    ) {
       return this.forbidden(res, null);
     }
 
