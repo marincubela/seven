dad268cff622c4003445e8df0a30add1e7499cf8
kristian240 <kristian.djakovic@infinum.hr>
2021-01-10 12:24:50 +0100

    Revert reservation list page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,6 +27,7 @@ import { ParkingEdit } from './routes/parkingsEdit';
 import { ProfileEditPerson } from './routes/profileEditPerson';
 import { ProfileEditCompany } from './routes/profileEditCompany';
 import { AdminRouter } from './Admin';
+import { ReservationList } from './routes/reservationList';
 import { AddReservation } from './routes/addReservation';
 import { AddOnetimeReservation } from './routes/addOnetimeReservation';
 import { AddRepetitiveReservation } from './routes/addRepetitiveReservation';
@@ -107,6 +108,10 @@ const App = () => {
 
             <Route path="/profile">
               <Profile />
+            </Route>
+
+            <Route path="/reservationList">
+              <ReservationList />
             </Route>
 
             <Route path="/addReservation">
---
+++ frontend/src/components/ReservationListPage/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ReservationListPage = ({ ...rest }) => {
+  return <div {...rest}>ReservationListPage</div>;
+};
